8c4b3dcc5bb641d7139b08fc89c5556cd10a5eaf
mandu5 <rhdudals0505@naver.com>
2023-07-14 21:51:50 +0900

    feat: text update

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="headerTab">
       <h1>군 적금계산기</h1>
-      <p>입금 금액과 기간을 입력하시면 수익률을 확인하실 수 있습니다.</p>
+      <p>입금 금액과 기간을 입력하시면 수익률을 확인하실 수 있습니다. </p>
     </div>
   );
 }
